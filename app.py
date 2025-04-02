2e2ac2833d24322e46146674f80f8c3ec8535f76
Nilesh Agarwal <nick.aggarwal@gmail.com>
2025-04-02 18:13:47 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import torch
 from io import BytesIO
 import base64
 import os
+import numpy as np
 
 class InferlessPythonModel:
     def initialize(self):
@@ -12,7 +13,8 @@ class InferlessPythonModel:
     def infer(self, inputs):
         prompt = inputs["prompt"]
         print("Hello World Promt 15")
-        return {'theta': [126.85237884521484], 'phi': [72.80898749828339], 'theta_confidence': [0.006437282077968121], 'phi_confidence': [0.013703356496989727], 'error': 'none'}
+        theta_deg = 126.8523788452148 
+        return {'theta': theta_deg.astype(np.float64), 'phi': [72.80898749828339], 'theta_confidence': [0.006437282077968121], 'phi_confidence': [0.013703356496989727], 'error': 'none'}
         
     def finalize(self):
         self.pipe = None
