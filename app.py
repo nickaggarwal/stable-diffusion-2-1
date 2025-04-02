dc2639ad286a742cd800d234bff1e24e858b357c
Nilesh Agarwal <nick.aggarwal@gmail.com>
2025-04-02 18:42:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -13,12 +13,12 @@ class InferlessPythonModel:
         prompt = inputs["prompt"]
         print("Hello World Promt 15")
         if prompt == "There is a fine house in the forest":
-            theta = [126.85237884521484]
+            theta = 126.85237884521484
         else:
             theta = [126.85237884521484, 126.85237884521484]
         return {
                 "theta": theta,
-                "phi": [90],
+                "phi": "Hello Wordl",
                 "theta_confidence": [126.85237884521484],
                 "phi_confidence": [12],
                 "error": "none"
